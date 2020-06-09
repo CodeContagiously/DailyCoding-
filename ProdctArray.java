d2a451666be9c63bbfc7714f68425d19440d81a9
PerryBoh <42015988+CodeContagiously@users.noreply.github.com>
2020-06-08 23:58:08 -0400

    Add files via upload

--- ProdctArray.java
+++ ProdctArray.java
@@ -5,11 +5,37 @@ import java.util.ArrayList;
 public class ProdctArray
 {
     static void main(String[] args)
-    {}
+    {
+        ArrayList<Integer> arr = new ArrayList<>();
+        int[] array = {1,2,3};
+        for(int num:array) arr.add(num);
+        ProdctArray ProdCl = new ProdctArray(arr);
+    }
+
     ArrayList<Integer> Array;
+    public ProdctArray(ArrayList<Integer> Array) //constructor for Product Array class
+    {
+        this.Array = Array;
+    }
+
     static void Eval(ArrayList<Integer> Array)
     {
-        //given an array return new array with elements at index i being product of all others except that.
-        
+        int ArrSize = Array.size();
+        ArrayList<Integer> NewList = new ArrayList<>();
+        for(int num=0; num<ArrSize; num++)
+        {
+            int result = 1;
+            ArrayList<Integer> subList1 = (ArrayList<Integer>) Array.subList(0,num);
+            ArrayList<Integer> subList2 = (ArrayList<Integer>) Array.subList(num,ArrSize);
+            subList1.addAll(subList2); //combine the two subList
+            for (int nums : subList1)
+            {
+                result *= nums;
+            }
+            NewList.add(result);
+
+        }
+        System.out.println(NewList);
+
     }
 }
