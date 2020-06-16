0214be9eac9aa221f3c805d9a0467b6a0c15014d
PerryBoh <42015988+CodeContagiously@users.noreply.github.com>
2020-06-16 02:47:41 -0400

    Add files via upload

---
+++ Shapes.java
@@ -0,0 +1,18 @@
+package DailyCoding;
+
+public abstract class Shapes
+{
+    public void Area()
+    {}
+}
+
+class Shape3D extends Shapes
+{
+
+    public Shape3D()
+    {
+        super();
+
+    }
+    public double volume(){}
+}
