8fb84723a8d7835939f76cd0984d036a393ecf7f
Afriq <42015988+CodeContagiously@users.noreply.github.com>
2020-06-26 13:56:22 -0400

    Add files via upload

--- BalanceBrackets.py
+++ BalanceBrackets.py
@@ -1,15 +1,20 @@
 def BracketBalance(stringBrackets):
     """This function computes whether a string of brackets have equal number of open and closed brackets"""
+    ##Check if all brackets come in pairs. If not, return False
     if "(" in stringBrackets:
-        if stringBrackets.count("(") == stringBrackets.count(")"): pass ##if condition is met pass and proceed to other brackets
-        else: return False
+        if stringBrackets.count("(") != stringBrackets.count(")"): return False##
     if "[" in stringBrackets:
-        if stringBrackets.count("[") == stringBrackets.count("]"): pass
-        else: return False
+        if stringBrackets.count("[") != stringBrackets.count("]"): return False
     if "{" in stringBrackets:
-        if stringBrackets.count("{") == stringBrackets.count("}"): return True ##on the last bracket return True if condition is met
-        else: return False
+        if stringBrackets.count("{") != stringBrackets.count("}"): return False ##
+   ##
+    openBracs, closeBracs, stack = ["(","[","{"], [")","]","}"], [] ##
+    for bracs in stringBrackets:
+        if bracs in openBracs: stack.append(bracs) ##add opening brackets to stack
+        else: ## else it's a close bracket so remove its corresponding open bracket
+            if stack.pop() != openBracs[closeBracs.index(bracs)]:return False ##check if its the corresponding open bracket
 
-"""Incomplete"""
+    if len(stack)!=0: return False
+    return True
 
-print(BracketBalance("([)]") )
+print(BracketBalance("{{[}}]") )
