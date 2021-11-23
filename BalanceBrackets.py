3865d9ab8d5e842f912198a5d9f7641cad29f338
Boh <42015988+boh-prog@users.noreply.github.com>
2021-11-23 07:15:46 -0500

    Update BalanceBrackets.py

--- BalanceBrackets.py
+++ BalanceBrackets.py
@@ -1,21 +1,32 @@
 def BracketBalance(stringBrackets):
-    """This function computes whether a string of brackets have equal number of open and closed brackets"""
-    ##Check if all brackets come in pairs. If not, return False
-    if "(" in stringBrackets:
-        if stringBrackets.count("(") != stringBrackets.count(")"): return False##
-    if "[" in stringBrackets:
-        if stringBrackets.count("[") != stringBrackets.count("]"): return False
-    if "{" in stringBrackets:
-        if stringBrackets.count("{") != stringBrackets.count("}"): return False ##
-   ##
-    openBracs, closeBracs, stack = ["(","[","{"], [")","]","}"], [] ##
-    for bracs in stringBrackets:
-        if bracs in openBracs: stack.append(bracs) ##add opening brackets to stack
-        else: ## else it's a close bracket so remove its corresponding open bracket
-            if stack.pop() != openBracs[closeBracs.index(bracs)]:return False ##check if its the corresponding open bracket
+    """This function computes whether a string of brackets have equal number of open and closed brackets and if they're in the right order"""
+    
+    ###################
+    if len(stringBrackets) == 1: return false ##if just one parantheses present in string
+    
+    stack = [] ##instantiate stack to hold parantheses
+    
+    for brac in stringBrackets:
+        if (brac == '{' or brac == '[' or brac == '('):
+            stack.append(brac) ##add every open parantheses in stack
+            
+        elif (c == '}' or c == ']' or c == ')'):
+            if len(stack) == 0: return False ##return false because there is no corresponding open parantheses in stack
+            
+            else:
+                ##check if each close parantheses has it's corresponding open parantheses in the right order. 
+                ##else return false
+                if (brac == ')' and  stack[-1]  == '('): stack.pop()
+                    
+                elif (brac == ']' and  stack[-1]  == '['): stack.pop()
 
-    if len(stack)!=0: return False
-    return True
+                elif (brac == '}' and stack[-1]  == '{'): stack.pop()
+
+                else: return false;
+
+       return len(stack)==0; ##if stack is empty return True, else it's false because of unequal number of parantheses.
+
+
 
 ####Test
 print(BracketBalance("{{[}}]") )
